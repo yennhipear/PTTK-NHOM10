1c4e27ae50a318905102fdbc342e61f62a066bc5
nhkduy <nhkduy201@gmail.com>
2022-04-19 14:28:43 +0700

    add to proj

---
+++ GUI/GUI/Menu.cs
@@ -0,0 +1,47 @@
+﻿using GUI.GUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GUI
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            (new DSVacxinGUI()).Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            (new DangKyLichRanhGUI()).Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            (new XepLichLamViecGUI()).Show();
+        }
+
+        private void btnDatMuaVacxin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
