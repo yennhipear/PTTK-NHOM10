ce59bc8dc0e1ffc21a7a0943dc033e85ae8d409f
nhkduy <nhkduy201@gmail.com>
2022-04-19 13:45:11 +0700

    duyet ds dat mua

---
+++ GUI/GUI/DuyetDSDatMua.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace GUI.GUI
+{
+    partial class DuyetDSDatMua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.vaccineName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.detail = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.isAccepted = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.vaccineName,
+            this.amount,
+            this.detail,
+            this.isAccepted});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(556, 379);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // vaccineName
+            // 
+            this.vaccineName.HeaderText = "Tên vắc xin";
+            this.vaccineName.MinimumWidth = 6;
+            this.vaccineName.Name = "vaccineName";
+            this.vaccineName.ReadOnly = true;
+            this.vaccineName.Width = 125;
+            // 
+            // amount
+            // 
+            this.amount.HeaderText = "Số lượng";
+            this.amount.MinimumWidth = 6;
+            this.amount.Name = "amount";
+            this.amount.ReadOnly = true;
+            this.amount.Width = 125;
+            // 
+            // detail
+            // 
+            this.detail.HeaderText = "Chi tiết";
+            this.detail.MinimumWidth = 6;
+            this.detail.Name = "detail";
+            this.detail.ReadOnly = true;
+            this.detail.Width = 125;
+            // 
+            // isAccepted
+            // 
+            this.isAccepted.HeaderText = "Được duyệt";
+            this.isAccepted.MinimumWidth = 6;
+            this.isAccepted.Name = "isAccepted";
+            this.isAccepted.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hoàn thành";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(473, 412);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Hủy";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // DuyetDSDatMua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 484);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "DuyetDSDatMua";
+            this.Text = "Duyệt danh sách đặt mua";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vaccineName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amount;
+        private System.Windows.Forms.DataGridViewButtonColumn detail;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isAccepted;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ GUI/GUI/DuyetDSDatMua.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.GUI
+{
+    public partial class DuyetDSDatMua : Form
+    {
+        public DuyetDSDatMua()
+        {
+            InitializeComponent();
+            List<string> vaccineNames = new List<string> { "Moderna", "Pfizer-BioNTech", "Varivax", "Tetanus" };
+        }
+    }
+}
