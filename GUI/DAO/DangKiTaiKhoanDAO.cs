585306d1a2c0e7f1bfb753e72be1f482bfc8803b
yennhipear <leyennhi112@gmail.com>
2022-05-05 16:53:16 +0700

    xong Dang ki :v

--- GUI/DAO/DangKiTaiKhoanDAO.cs
+++ GUI/DAO/DangKiTaiKhoanDAO.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace GUI.DAO
 {
@@ -13,9 +15,36 @@ namespace GUI.DAO
         SqlCommand sqlCommand; //de truy van cac cau lenh insert, update, delete...
         SqlDataReader dataReader; //doc data trong table
 
-        //public List <TaiKhoan> TaiKhoans (string query)
-        //{
+        public List<DTO.TaiKhoanDTO> TaiKhoans(string query)
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
 
-        //}
+            List<DTO.TaiKhoanDTO> taikhoans = new List<DTO.TaiKhoanDTO>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(query, sqlConnection);
+                dataReader = sqlCommand.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    taikhoans.Add(new DTO.TaiKhoanDTO(dataReader.GetString(0), dataReader.GetString(1), dataReader.GetString(2), dataReader.GetString(3), dataReader.GetString(4), dataReader.GetString(5), dataReader.GetString(6), dataReader.GetString(7), dataReader.GetString(8), dataReader.GetDateTime(9)));
+                }
+                sqlConnection.Close();
+            }
+            return taikhoans;
+        }
+
+        public void Command(string query) //de dang ki taikhoan
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
     }
 }
---
+++ GUI/DTO/TaiKhoanDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.DTO
+{
+    class TaiKhoanDTO
+    {
+        string username;
+        string password;
+        string confirmPass;
+        string name;
+        string gender;
+        string cmnd;
+        string sdt;
+        string email;
+        string diachi;
+        DateTime ngaysinh;
+
+        public TaiKhoanDTO() { }
+        public TaiKhoanDTO(string username, string password, string confirmPass, string name, string gender, string cmnd, string sdt, string email, string diachi, DateTime ngaysinh)
+        {
+            this.username = username;
+            this.password = password;
+            this.confirmPass = confirmPass;
+            this.name = name;
+            this.gender = gender;
+            this.cmnd = cmnd;
+            this.sdt = sdt;
+            this.email = email;
+            this.diachi = diachi;
+            this.ngaysinh = ngaysinh;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string ConfirmPass { get => confirmPass; set => confirmPass = value; }
+        public string Name { get => name; set => name = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string CMND { get => cmnd; set => cmnd = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
+        public string Email { get => email; set => email = value; }
+        public string Diachi { get => diachi; set => diachi = value; }
+        public DateTime Ngaysinh { get => ngaysinh; set => ngaysinh = value; }
+    }
+
+}
